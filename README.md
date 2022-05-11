8b15ebe898773cb9a6ae22d3065ed484fda16021
jairpro <jairedias@yahoo.com.br>
2022-05-11 20:38:33 -0300

    Aula 3 - nodemailer

--- README.md
+++ README.md
@@ -3,23 +3,27 @@
 ### Tecnologias
 [Prisma](https://prisma.io) |
 [Prisma/SQLite](https://www.prisma.io/docs/concepts/database-connectors/sqlite) |
+[mailtrap](https://mailtrap.io/)
 
 ### Criação do projeto:
+      npm init -y
       npm i typescript @types/node ts-node-dev -D
-
       npx tsc --init
-
       npx tsc
 
 ### Execução
       npm run dev
 
 ### Dependências
+      npm i express
+      npm i @types/express -D
+
       npm i prisma -D
       npm i @prisma/client
       npx prisma init
-
       npx prisma migrate dev
-      ...create_feedbacks
-
+        ...create_feedbacks
       npx prisma studio
+
+      npm install nodemailer
+      npm install @types/nodemailer -D
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Example <oi@example.com>',
+    to: 'Dev Example <dev@example.com',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
   })
   
   return res.status(201).json({ data: feedback })
