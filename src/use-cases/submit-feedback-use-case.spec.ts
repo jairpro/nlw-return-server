2d94f3dfa82af8983723bd5615e7a96fa65c2191
jairpro <jairedias@yahoo.com.br>
2022-05-13 16:22:05 -0300

    testa chamada dos métodos: createFeedback e sendMail

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,17 +1,23 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
+const createFeedbackSpy = jest.fn()
+const sendMailSpy = jest.fn()
+
 const submitFeedback = new SubmitFeedbackUseCase(
-  { create: async () => {} },
-  { sendMail: async () => {} }
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
 )
 
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
-   await expect(submitFeedback.execute({
+    await expect(submitFeedback.execute({
       type: 'BUG',
       comment: 'example comment',
       screenshot: 'data:image/png;base64,dkjghdfjgfhdlgkjdfhlgkjdfhg',
     })).resolves.not.toThrow()
+
+    expect(createFeedbackSpy).toHaveBeenCalled()
+    expect(sendMailSpy).toHaveBeenCalled()
   })
 
   it('should be able to submit a feedback without type', async () => {
